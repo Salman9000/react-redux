b838a5a7284ea3c63b0c1e757f848e72f604e290
Salman Hanif <salmanhanif133@gmail.com>
2021-12-06 17:54:20 +0500

    first commit

---
+++ src/components/TutorialsList.js
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  retrieveTutorials,
+  findTutorialsByTitle,
+  deleteAllTutorials,
+} from "../actions/tutorials";
+import { Link } from "react-router-dom";
+
+const TutorialsList = () => {
+  const [currentTutorial, setCurrentTutorial] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+  const [searchTitle, setSearchTitle] = useState("");
+
+  const tutorials = useSelector((state) => state.tutorials);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(retrieveTutorials());
+  }, []);
+
+  const onChangeSearchTitle = (e) => {
+    const searchTitle = e.target.value;
+    setSearchTitle(searchTitle);
+    findByTitle(searchTitle)
+  };
+
+  const refreshData = () => {
+    setCurrentTutorial(null);
+    setCurrentIndex(-1);
+  };
+
+  const setActiveTutorial = (tutorial, index) => {
+    setCurrentTutorial(tutorial);
+    setCurrentIndex(index);
+  };
+
+  const removeAllTutorials = () => {
+    dispatch(deleteAllTutorials())
+      .then((response) => {
+        refreshData();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const findByTitle = (searchValue) => {
+    refreshData();
+    dispatch(findTutorialsByTitle(searchValue));
+  };
+
+  return (
+    <div className="list row">
+      <div className="col-md-8">
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search by title"
+            value={searchTitle}
+            onChange={onChangeSearchTitle}
+          />
+          <div className="input-group-append">
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              onClick={findByTitle}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-6">
+        <h4>Tutorials List</h4>
+
+        <ul className="list-group">
+          {tutorials &&
+            tutorials.map((tutorial, index) => (
+              <li
+                className={
+                  "list-group-item " +  (searchTitle?.length >=1 && index !== currentIndex ?  "bg-warning bg-opacity-10 " : "") + (index === currentIndex ? "active" : "")
+                }
+                onClick={() => setActiveTutorial(tutorial, index)}
+                key={index}
+              >
+                {tutorial.title}
+              </li>
+            ))}
+        </ul>
+
+        {searchTitle?.length === 0 && <button
+          className="m-3 btn btn-sm btn-danger"
+          onClick={removeAllTutorials}
+        >
+          Remove All
+        </button>}
+      </div>
+      <div className="col-md-6">
+        {currentTutorial ? (
+          <div>
+            <h4>Tutorial</h4>
+            <div>
+              <label>
+                <strong>Title:</strong>
+              </label>{" "}
+              {currentTutorial.title}
+            </div>
+            <div>
+              <label>
+                <strong>Description:</strong>
+              </label>{" "}
+              {currentTutorial.description}
+            </div>
+            <div>
+              <label>
+                <strong>Status:</strong>
+              </label>{" "}
+              {currentTutorial.published ? "Published" : "Pending"}
+            </div>
+
+            <Link to={"/tutorials/" + currentTutorial.id}>
+              <span className="badge bg-warning">Edit</span>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <br />
+            <p>Please click on a Tutorial...</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TutorialsList;
